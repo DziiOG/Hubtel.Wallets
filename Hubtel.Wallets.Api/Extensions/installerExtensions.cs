f30eed09c82b78838915a885d4cf9e971b11728a
Pontiff Sulyvahn <workwithwhitson@gmail.com>
2023-02-15 14:54:42 +0000

    changes

--- Hubtel.Wallets.Api/Extensions/installerExtensions.cs
+++ Hubtel.Wallets.Api/Extensions/installerExtensions.cs
@@ -13,26 +13,31 @@ namespace Hubtel.Wallets.Api.Extensions
     public static class InstallerExtensions
     {
         /// <summary>
-        /// Method excutes all class implementation of the IInstaller interface which are not abstracts or interfaces themselves.
+        /// This method allows for modular service installation by scanning the assembly for installers that implement the IInstaller interface.
+        /// This makes it easy to add or remove services by simply adding or removing an installer.
         /// </summary>
         /// <param name="services">Specifies the contract for a collection of service descriptors.</param>
         /// <param name="configuration">Represents a set of key/value application configuration properties.</param>
-        /// <returns>Task</returns>
+        /// <returns>A task representing the completion of the service installation.</returns>
         public static void InstallServicesInAssembly(
-            this IServiceCollection services,
-            IConfiguration configuration
+            this IServiceCollection services, // The IServiceCollection to install services into
+            IConfiguration configuration // The IConfiguration object containing application configuration properties
         )
         {
-            List<IInstaller> Installers = typeof(Program).Assembly.ExportedTypes.Where(
+            // Get all exported types in the assembly that contains the Program class
+            // Filter the list to only include types that implement IInstaller, are not abstract or interface types
+            List<IInstaller> Installers = typeof(Program).Assembly.ExportedTypes
+                .Where(
                     exportedType =>
-                        typeof(IInstaller).IsAssignableFrom(exportedType)
-                        && !exportedType.IsInterface
-                        && !exportedType.IsAbstract
+                        typeof(IInstaller).IsAssignableFrom(exportedType) // Check if type implements IInstaller
+                        && !exportedType.IsInterface // Check if type is not an interface
+                        && !exportedType.IsAbstract // Check if type is not abstract
                 )
-                .Select(Activator.CreateInstance)
-                .Cast<IInstaller>()
-                .ToList();
+                .Select(Activator.CreateInstance) // Create an instance of each filtered type
+                .Cast<IInstaller>() // Cast the instance to the IInstaller interface
+                .ToList(); // Convert the filtered and cast instances to a List<IInstaller>
 
+            // Call InstallServices on each IInstaller instance in the list to install services into the IServiceCollection
             Installers.ForEach(Installer => Installer.InstallServices(services, configuration));
         }
     }
